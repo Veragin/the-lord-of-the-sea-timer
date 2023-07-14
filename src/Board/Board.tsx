29dba453823f5186a512a18786163d7126ff0a11
Tan Tichavský <tichavsk@seznam.cz>
2023-07-14 07:56:46 +0200

    icons added

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,11 +1,20 @@
 import { Column } from 'Components/StyledComponents';
 import { observer } from 'mobx-react-lite';
 import { StateBar } from './StateBar';
+import styled from 'styled-components';
+import { spacingCss } from 'Components/globalCss';
 
 export const Board = observer(() => {
     return (
-        <Column>
+        <StyledColumn>
             <StateBar />
-        </Column>
+        </StyledColumn>
     );
 });
+
+const StyledColumn = styled(Column)`
+    flex: 1;
+    width: calc(100% - ${spacingCss(8)});
+    padding: ${spacingCss(4)};
+    background-color: #20b2aa;
+`;
--- src/Board/StateBar.tsx
+++ src/Board/StateBar.tsx
@@ -1,4 +1,7 @@
-import { PauseCircleFilledRounded, PlayCircleOutlineRounded } from '@mui/icons-material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'Components/Buttons';
 import { Row } from 'Components/StyledComponents';
 import { useStore } from 'Context/StoreContext';
@@ -13,18 +16,34 @@ export const StateBar = observer(() => {
     return (
         <StyledRow>
             <Button onClick={() => store.setState(isPaused ? 'runnning' : 'paused')}>
-                {isPaused ? <PlayCircleOutlineRounded /> : <PauseCircleFilledRounded />}
+                {isPaused ? (
+                    <FontAwesomeIcon icon={faPlay} size={'2x'} />
+                ) : (
+                    <FontAwesomeIcon icon={faPause} size={'2x'} />
+                )}
             </Button>
             <StyledTitle $isPaused={isPaused}>{isPaused ? 'PAUSED' : ''}</StyledTitle>
+            <Button
+                onClick={() => {
+                    // eslint-disable-next-line no-restricted-globals
+                    if (confirm('Are you sure you want to exit?')) {
+                        store.setWasStarted(false);
+                    }
+                }}
+            >
+                <FontAwesomeIcon icon={faClose} size={'2x'} />
+            </Button>
         </StyledRow>
     );
 });
 
 const StyledRow = styled(Row)`
     justify-content: space-between;
+    width: 100%;
 `;
 
 const StyledTitle = styled.span<{ $isPaused: boolean }>`
-    font-size: 50px;
-    color: ${({ $isPaused }) => ($isPaused ? 'orange' : 'white')};
+    font-size: 55px;
+    font-weight: 600;
+    color: ${({ $isPaused }) => ($isPaused ? 'gold' : 'white')};
 `;
